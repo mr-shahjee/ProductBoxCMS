e90e081099b0bf5d3f79afcbc27a982747601e5f
Abdullah Shah <abdullahshahjee7@gmail.com>
2026-03-17 20:51:20 +0500

    implemented unit testing

---
+++ Test/Tests/Services/CustomerServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+using ReactAndAspApp.Server.Models;
+using ReactAndAspApp.Server.Repositories;
+using ReactAndAspApp.Server.Services;
+using Xunit;
+using FluentAssertions;
+
+public class CustomerServiceTests
+{
+    private readonly Mock<ICustomerRepository> _repoMock;
+    private readonly CustomerService _service;
+
+    public CustomerServiceTests()
+    {
+        _repoMock = new Mock<ICustomerRepository>();
+        _service = new CustomerService(_repoMock.Object);
+    }
+
+    [Fact]
+    public async Task GetAllAsync_ShouldReturnCustomers()
+    {
+        // Arrange
+        var data = new List<Customer>
+    {
+        new Customer { Id = 1, Name = "Ali" }
+    };
+
+        _repoMock.Setup(x => x.GetAllWithTypeAsync())
+                 .ReturnsAsync(data);
+
+        // Act
+        var result = await _service.GetAllAsync();
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().HaveCount(1);
+    }
+}
---
+++ Test/Tests/Services/CustomerTypeServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Moq;
+using ReactAndAspApp.Server.Models;
+using ReactAndAspApp.Server.Repositories;
+using ReactAndAspApp.Server.Services;
+using Xunit;
+
+public class CustomerTypeServiceTests
+{
+    private readonly Mock<ICustomerTypeRepository> _repoMock;
+    private readonly CustomerTypeService _service;
+
+    public CustomerTypeServiceTests()
+    {
+        _repoMock = new Mock<ICustomerTypeRepository>();
+        _service = new CustomerTypeService(_repoMock.Object);
+    }
+
+    [Fact]
+    public async Task GetAllAsync_ShouldReturnTypes()
+    {
+        var types = new List<CustomerType>
+        {
+            new CustomerType { Id = 1, Name = "Regular" }
+        };
+
+        _repoMock.Setup(x => x.GetAllAsync())
+                 .ReturnsAsync(types);
+
+        var result = await _service.GetAllAsync();
+
+        result.Should().HaveCount(1);
+    }
+}
